cb9f8630b7ceb6f3287ec00fe2009a62b28abefa
AntonioLopga12 <antoniolopgalvear@icloud.com>
2022-01-19 19:27:37 +0100

    up

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,90 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
+
+print(np.version.version)
 
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random(size=(2,3,5))
+
+
 
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.random.random(size=(5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
 
+print(np.size(a) == np.size(b),', Yes, the size of a is ',np.size(a),', and the size of b is ', np.size(b))
 
 
 #8. Are you able to add a and b? Why or why not?
+
+print(a.shape == b.shape,', Is not possible to merge because the shape of a is,',a.shape,' and the shape of b is,',b.shape,' they have different rows and columns.')
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.transpose(1,2,0)
+print(c)
+print(c.shape)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
 
+d = a * c
+
+print('Ii works, because the shape of a is, ', a.shape,'and its size is, ',a.size,' the same as c that has a shape of, ',c.shape,'size of, ',c.size,'.')
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a)
+print(b)
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
 
 
+print('a have a size of, ',a.size, ' and a shape of,',a.shape,'and e has a size of, ',e.size,'and shape of, ',e.shape,'both share size and shape but differ in values',a == e) 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
+
+print('The max number in b is, ',d_max,'the min number is ', d_min,'and the mean is ',d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
 
-
+f = np.empty([2,3,5])
+print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
