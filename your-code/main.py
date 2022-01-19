6b2ff524393278639055fec4fa2568db7479e637
ce-valle <zevalle@gmail.com>
2022-01-19 16:30:31 +0100

    menos el bonus

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,70 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print(np.version.version)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a = np.random.random((2,3,5))
+a2 = np.random.randint((2,3,5))
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,2,3), dtype=int)
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print(a.size == b.size)
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
-
+#no se puede porque aunque tengan el mismo size, no tienen el mismo shape
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.transpose(1,2,0)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = a + c
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print(f"a ={a}")
+print(f"d ={d}")
+#los valores de "d" son los mismos que los de "a" + 1
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a * c
+print(f"e = {e}")
 
 #13. Does e equal to a? Why or why not?
-
+#al ser todo 1s los valores de c, al multiplicarlos por a, queda el mismo valor
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
+print(f"max={d_max}")
+print(f"min={d_min}")
+print(f"mean={d_mean}")
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
+f = np.empty((2,3,5))
+print(f"f={f}")
 
 
 """
