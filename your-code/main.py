0a4e5c1a38d3285c0be36bfa7a89622966d52819
Pablo Mora <pablo.mora213@gmail.com>
2022-01-19 18:10:54 +0100

    mensaje

--- your-code/main.py
+++ your-code/main.py
@@ -1,68 +1,77 @@
 #1. Import the NUMPY package under the name np.
 
+import numpy as np 
 
 
 #2. Print the NUMPY version and the configuration.
 
 
 
+
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+
+a = np.random.random((2, 3, 5))
+
 
 
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b=np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+  #yes
+a.size==b.size
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+    #no, no tienen la misma forma
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c= b.transpose((1,2,0))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d= c+a
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a)
+print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e= a*c
 
 #13. Does e equal to a? Why or why not?
 
-
+e==a
+#yes, its the same as multiply by one
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
 
-
+d_max=d.max()
+d_min=d.min()
+d_mean=d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f=np.empty((2,3,5))
 
 
 """
@@ -74,8 +83,21 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
-
+l=[]
+for z in d:
+    for x in z:
+        for i in x:
+            if i == d_min:
+                l.append(0)
+            elif i == d_mean:
+                l.append(50)
+            elif i == d_max:
+                l.append(100)
+            elif i > d_mean and i < d_max:
+                l.append(75)
+            elif i > d_min and i< d_mean:
+                l.append(25) 
+f= np.array(l).reshape(2,3,5)
 
 
 """
@@ -98,7 +120,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print(d)
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +135,20 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+ultima=[]
+for zl in d:
+    for xl in zl:
+        for il in xl:
+            if il == d_min:
+                ultima.append("A")
+            elif il == d_mean:
+                ultima.append("C")
+            elif il == d_max:
+                ultima.append("E")
+            elif il > d_mean and i < d_max:
+                ultima.append("D")
+            elif il > d_min and i< d_mean:
+                ultima.append("B") 
+bonus= np.array(ultima).reshape(2,3,5)
+print(bonus)
