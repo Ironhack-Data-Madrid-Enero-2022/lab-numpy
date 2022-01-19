c1b95caed9bd87515cd7ed350ccb0e0167420a9b
mariozapatero <mario_zc@hotmail.com>
2022-01-19 18:49:31 +0100

    lab con soluciones DEF

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,104 @@
 #1. Import the NUMPY package under the name np.
 
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2,3,5))
 
+aa = np.random.randint(10, size=(2,3,5))
+
+aaa = np.random.randint(low=10, high=21, size=(2,3,5))
 
 #4. Print a.
 
+print(a)
 
+print(aa)
+
+print(aaa)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+print(a.size == b.size)
 
+"""Nos devolverá true si a y b tienen el mismo size."""
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+""""No podemos hacer operaciones con a y b porque no tienen el mismo shape (ni siquiera el mismo size en este caso)
+a.shape == b.shape ---> False"""
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = b.transpose(1,2,0)
 
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = a + c
 
+print(d)
+
+""" Sí, parece que funciona sin problemas (suma 1 a todos los valores de "a")"""
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
 
+print("Array a")
+
+print(a)
+
+print("Array d")
+
+print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e = a * c
 
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
+print(a == e)
 
-
+""" Sí, las matrices "a" y "e" son iguales.
+Esto pasa porque: e = a * c --> Todos los valores de c son 1 ; por lo que el resultado de la multplicación es el mismo."""
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
-
+print(d_max)
+print(d_min)
+print(d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty((2,3,5))
+print(f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -75,6 +110,19 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for s in range(2):
+        for z in range(3):
+                for i in range(5):
+                        if d[s][z][i] > d_min and d[s][z][i] < d_mean:
+                                f[s][z][i] = 25
+                        elif d[s][z][i] > d_mean and d[s][z][i] < d_max:
+                                f[s][z][i] = 75
+                        elif d[s][z][i] == d_mean:
+                                f[s][z][i] = 50
+                        elif d[s][z][i] == d_min:
+                                f[s][z][i] = 0
+                        elif d[s][z][i] == d_max:
+                                f[s][z][i] = 100
 
 
 
@@ -98,7 +146,10 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print("Esto es d:")
+print(d)
+print("Esto es f:")
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +163,24 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+f_con_strings = np.empty((2,3,5), dtype = str)
+
+for s in range(2):
+        for z in range(3):
+                for i in range(5):
+                        if d[s][z][i] > d_min and d[s][z][i] < d_mean:
+                                f_con_strings[s][z][i] = "B"
+                        elif d[s][z][i] > d_mean and d[s][z][i] < d_max:
+                                f_con_strings[s][z][i] = "D"
+                        elif d[s][z][i] == d_mean:
+                                f_con_strings[s][z][i] = "C"
+                        elif d[s][z][i] == d_min:
+                                f_con_strings[s][z][i] = "A"
+                        elif d[s][z][i] == d_max:
+                                f_con_strings[s][z][i] = "E"
+
+
+print("Esto es f_con_strings")
+print(f_con_strings)
+
